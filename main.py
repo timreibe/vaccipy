72d6b457126e7288f67502e681ceb79dc3c64c9b
Matze1x <67190626+Matze1x@users.noreply.github.com>
2021-05-15 18:38:57 +0200

    Add files via upload
    support raspbian

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ import requests
 from plyer import notification
 from selenium.webdriver import ActionChains
 from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+options = Options()
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--start-maximized")
+options.add_argument("--headless")
+options.add_argument("--log-level=0")
+options.add_argument("--enable-logging=stderr")
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
