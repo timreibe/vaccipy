e866de5954a9da1280f52cab6081697d0919c37d
Floskinner <58706771+Floskinner@users.noreply.github.com>
2021-05-30 16:05:09 +0200

    neuer FilePicker verwenden wenn es Windows ist

--- tools/gui/__init__.py
+++ tools/gui/__init__.py
@@ -1,5 +1,6 @@
 import os
 import json
+import platform
 from enum import Enum, auto
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import QMessageBox
@@ -92,12 +93,16 @@ def oeffne_file_dialog_save(parent_widged: QtWidgets.QWidget, titel: str, standa
         str: Vollständiger Pfad
     """
 
+    os_name: str = platform.system()
     options = QtWidgets.QFileDialog.Options()
-    options |= QtWidgets.QFileDialog.DontUseNativeDialog
+
+    if os_name.lower() != "windows":
+        options |= QtWidgets.QFileDialog.DontUseNativeDialog
+
     datei_data = QtWidgets.QFileDialog.getSaveFileName(parent=parent_widged, caption=titel, directory=standard_speicherpfad, filter=dateityp, options=options)
     dateipfad = datei_data[0]  # (Pfad, Dateityp)
 
-    dateipfad = dateipfad.replace("/", os.path.sep)
+    dateipfad = dateipfad.replace("/", os_name.path.sep)
 
     if not dateipfad:
         raise FileNotFoundError
@@ -123,8 +128,11 @@ def oeffne_file_dialog_select(parent_widged: QtWidgets.QWidget, titel: str, stan
     """
 
     # Öffnet den "File-Picker" vom System um ein bereits existierende Datei auszuwählen
+    os_name: str = platform.system()
     options = QtWidgets.QFileDialog.Options()
-    options |= QtWidgets.QFileDialog.DontUseNativeDialog
+    if os_name.lower() != "windows":
+        options |= QtWidgets.QFileDialog.DontUseNativeDialog
+
     datei_data = QtWidgets.QFileDialog.getOpenFileName(parent=parent_widged, caption=titel, directory=standard_oeffnungspfad, filter=dateityp, options=options)
     dateipfad = datei_data[0]  # (pfad, typ)
 
