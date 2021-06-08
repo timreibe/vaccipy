9e6d14088c3cb639f28237ddde8a242a7971239e
Sebastian Hasler <sebastian.hasler@gmx.net>
2021-06-08 18:33:30 +0200

    feat(code): Allow to specify multiple codes

--- tools/exceptions.py
+++ tools/exceptions.py
@@ -1,10 +1,30 @@
+class AppointmentGone(Exception):
+    pass
+
+
+class BookingError(Exception):
+    pass
+
+
 class DesktopNotificationError(Exception):
     pass
 
 
-class ValidationError(Exception):
+class LoginFailed(Exception):
     pass
 
 
 class MissingValuesError(Exception):
+    pass
+
+
+class TimeframeMissed(Exception):
+    pass
+
+
+class UnmatchingCodeError(Exception):
+    pass
+
+
+class ValidationError(Exception):
     pass
